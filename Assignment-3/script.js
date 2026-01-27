99b9f0acafc74a8c253bb16375906faf2d630cb1
NitishChowdaryK <kolupoti.n@northeastern.edu>
2026-01-27 17:47:51 -0500

    added green arrow feature to open and collapse

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -1,38 +1,48 @@
 //Title constructor function that creates a Title object
-function Title(t1) 
-{ this.mytitle = t1;
+function Title(t1) {
+  this.mytitle = t1
 }
 
-Title.prototype.getName = function () 
-{ 
-return (this.mytitle);
+Title.prototype.getName = function () {
+  return this.mytitle
 }
 
 var socialMedia = {
-  facebook : 'http://facebook.com',
+  facebook: 'http://facebook.com',
   twitter: 'http://twitter.com',
   flickr: 'http://flickr.com',
-  youtube: 'http://youtube.com'
-};
+  youtube: 'http://youtube.com',
+}
 
-var t = new Title("CONNECT WITH ME!");
-
-
-
+var t = new Title('CONNECT WITH ME!')
 
 window.onload = function () {
+  const header = document.createElement('h2')
+  header.innerText = 'Full Name: Nitish Chowdary Kolupoti | NUID: 002306783'
+  document.body.insertBefore(header, document.body.firstChild)
 
-  const header = document.createElement("h2");
-  header.innerText = "Full Name: Nitish Chowdary Kolupoti | NUID: 002306783";
-  document.body.insertBefore(header, document.body.firstChild);
+  const dropDowns = document.querySelectorAll('.dropDownTextArea')
+  dropDowns.forEach((row) => {
+    row.style.display = 'none'
+  })
 
-  const dropDowns = document.querySelectorAll(".dropDownTextArea");
-  dropDowns.forEach(row => {
-    row.style.display = "none";
-  });
+  const submitBtn = document.getElementById('button')
+  submitBtn.disabled = true
+  submitBtn.style.backgroundColor = 'gray'
+  submitBtn.style.cursor = 'not-allowed'
+}
 
-  const submitBtn = document.getElementById("button");
-  submitBtn.disabled = true;
-  submitBtn.style.backgroundColor = "gray";
-  submitBtn.style.cursor = "not-allowed";
-};
+document.addEventListener('click', function (event) {
+  if (event.target.tagName === 'IMG' && event.target.src.includes('down.png')) {
+    const currentRow = event.target.closest('tr')
+    const detailRow = currentRow.nextElementSibling
+
+    if (detailRow && detailRow.classList.contains('dropDownTextArea')) {
+      if (detailRow.style.display === 'none') {
+        detailRow.style.display = 'table-row'
+      } else {
+        detailRow.style.display = 'none'
+      }
+    }
+  }
+})
