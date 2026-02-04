828517fc01de8d59d3ef497c20222356ecf5615e
NitishChowdaryK <kolupoti.n@northeastern.edu>
2026-02-03 22:25:03 -0500

    added error place holders and styling for validation feedback

--- Assignment_04/script.js
+++ Assignment_04/script.js
@@ -6,3 +6,20 @@ window.onload = function () {
   submitBtn.style.cursor = "not-allowed";
   submitBtn.style.opacity = "0.6";
 };
+
+
+function setError(inputEl, errEl, msg) {
+  errEl.innerText = msg;
+  if (inputEl) {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+}
+
+function clearError(inputEl, errEl) {
+  errEl.innerText = "";
+  if (inputEl) {
+    inputEl.classList.remove("invalid");
+    inputEl.classList.add("valid");
+  }
+}
